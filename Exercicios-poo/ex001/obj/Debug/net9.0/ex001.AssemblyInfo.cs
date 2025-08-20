16a155a38e06157eeba146095bf9fc71faa6504c
iRasec <luancesar@alunos.utfpr.edu.br>
2025-08-19 23:41:27 -0300

    conclusão dos exercicios que faltaram

--- Exercicios-poo/ex001/obj/Debug/net9.0/ex001.AssemblyInfo.cs
+++ Exercicios-poo/ex001/obj/Debug/net9.0/ex001.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e5800c0b62ddbf5033c4137d45fd8274e3458f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
